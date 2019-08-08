b631a34cccdea7d68d33de70c9251c8500b1f2d6
Student <example@example.com>
2019-08-08 15:33:31 -0700

    ships are animating

--- PiratesDataBases/Controllers/PlayController.cs
+++ PiratesDataBases/Controllers/PlayController.cs
@@ -39,3 +39,24 @@ namespace PiratesDataBases.Controllers
 }
 
 //how to do this properly?
+
+/*
+var geometry = new THREE.BoxGeometry( thisShip.hp / 100, .2, .2 );
+            if (shipPiece.team == 1){
+                var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+            } else if (shipPiece.team == 2){
+                var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );  
+            } else {
+                var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );  
+            }
+			
+            var cube = new THREE.Mesh( geometry, material );
+            cube.position.x = shipPiece.xPos;
+            cube.position.y = shipPiece.yPos;
+            
+            
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+
+scene.add( cube );
+ */
